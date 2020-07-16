6766ed7bd80eca2eca8d984b245c2e0184c9fba1
Kevin Proudler <kevinproudler@gmail.com>
2020-07-16 09:23:18 -0400

    store test

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 function Root() {
+    // const store = configureStore();
+    // window.store = store
     return(
         <h1>Todos!</h1>
     )
